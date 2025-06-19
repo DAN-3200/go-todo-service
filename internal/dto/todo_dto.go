d00e1e24151aa37e1207ded28c1d236abe283bc6
Daniel <danmoreira.dev@gmail.com>
2025-06-19 13:03:12 -0300

    feat: adiciona a camada de dto para transição de dados entre camadas

---
+++ internal/dto/todo_dto.go
@@ -0,0 +1,24 @@
+// DTO (Data Transfer Object)
+package dto
+
+import "time"
+
+type ToDoReq struct {
+	Title   string `json:"title" binding:"required"`
+	Content string `json:"content" binding:"required"`
+	Status  bool   `json:"status"`
+}
+
+type ToDoRes struct {
+	ID        int64       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Status    bool      `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type ToDoEditReq struct {
+	Title   *string `json:"title"`
+	Content *string `json:"content"`
+	Status  *bool   `json:"status"`
+}
