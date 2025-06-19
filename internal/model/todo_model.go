541a9bbc717e570e114cd068367210a7287f97df
Daniel <danmoreira.dev@gmail.com>
2025-06-19 12:59:33 -0300

    feat: incrementa campo createAt no model ToDo

---
+++ internal/model/todo_model.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type ToDo struct {
+	ID        int64       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Status    bool      `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+}
